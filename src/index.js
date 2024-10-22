9f953e76f21463de7fa725892da922c67084471b
Gsszs <gabriel.selvenca@icloud.com>
2024-10-22 17:20:05 -0300

    Finalização att 2.0

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const client = new Client({
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.GuildMessageReactions,
         IntentsBitField.Flags.MessageContent,
-        IntentsBitField.Flags.GuildPresences
+        IntentsBitField.Flags.GuildPresences,
+        IntentsBitField.Flags.GuildEmojisAndStickers,
     ]
 });
 
@@ -30,17 +31,21 @@ async function createThreadIfNeeded(message) {
             } else if (message.attachments.size > 0) {
                 const guild = message.guild;
 
-                const correctEmoji = await guild.emojis.fetch(correctEmojiID);
-                const incorrectEmoji = await guild.emojis.fetch(incorrectEmojiID);
+                const correctEmoji = guild.emojis.cache.get(correctEmojiID);
+                const incorrectEmoji = guild.emojis.cache.get(incorrectEmojiID);
 
-                await message.react(correctEmoji);
-                await message.react(incorrectEmoji);
+                if (correctEmoji && incorrectEmoji) {
+                    await message.react(correctEmoji);
+                    await message.react(incorrectEmoji);
 
-                await message.startThread({
-                    name: `${message.author.username} post`,
-                    autoArchiveDuration: 1440,
-                    reason: '',
-                });
+                    await message.startThread({
+                        name: `${message.author.username} post`,
+                        autoArchiveDuration: 1440,
+                        reason: '',
+                    });
+                } else {
+                    console.error("Erro: Emoji não encontrado!");
+                }
             } else {
                 await message.delete();
                 const sentMessage = await message.channel.send(`<@${message.author.id}> você não pode enviar mensagens sem arquivos nesse canal!`);
@@ -65,59 +70,53 @@ async function createThreadIfNeeded(message) {
 }
 
 const sendStatusUpdate = async () => {
-    try {
-        const channel = await client.channels.fetch(pingChat);
+    const channel = await client.channels.fetch(pingChat);
 
-        if (!channel) {
-            console.error("Canal para envio de status não encontrado.");
-            return;
-        }
+    if (!channel) {
+        console.error("Canal para envio de status não encontrado.");
+        return;
+    }
 
-        const uptime = process.uptime();
-        const days = Math.floor(uptime / 86400);
-        const hours = Math.floor((uptime % 86400) / 3600);
-        const minutes = Math.floor((uptime % 3600) / 60);  
-        const seconds = Math.floor(uptime % 60);
-        const formattedUptime = `__**${days}**d **${hours}**h **${minutes}**m **${seconds}**s__`;
+    const uptime = process.uptime();
+    const days = Math.floor(uptime / 86400);
+    const hours = Math.floor(uptime / 3600);
+    const minutes = Math.floor((uptime % 3600) / 60);
+    const seconds = Math.floor(uptime % 60);
+    const formattedUptime = `__**${days}**d **${hours}**h **${minutes}**m **${seconds}**s__`;
 
-        const memoryUsage = process.memoryUsage().heapUsed / 1024 / 1024;
-        const formattedMemoryUsage = `${Math.round(memoryUsage * 100) / 100} MB`;
+    const memoryUsage = process.memoryUsage().heapUsed / 1024 / 1024;
+    const formattedMemoryUsage = `${Math.round(memoryUsage * 100) / 100} MB`;
 
-        const serverCount = client.guilds.cache.size;
-        const embed = new EmbedBuilder()
-            .setColor('#0099ff')
-            .setTitle(`**Bot Status - 1.2 (beta)**`)
-            .addFields(
-                { name: '🌐 Status', value: isBotOnline ? `Online` : `Offline`, inline: false },
-                { name: '🕒 Uptime', value: `${formattedUptime}\n`, inline: false },
-                { name: '🏠 Servers count', value: `${serverCount}\n`, inline: false },
-                { name: '💾 Memory use', value: `${formattedMemoryUsage}\n`, inline: false }
-            )
-            .setTimestamp();
+    const serverCount = client.guilds.cache.size;
+    const embed = new EmbedBuilder()
+        .setColor('#0099ff')
+        .setTitle(`**Bot Status - ${client.user.tag}**`)
+        .addFields(
+            { name: '🌐 Status', value: isBotOnline ? `Online` : `Offline`, inline: false },
+            { name: '🕒 Uptime', value: `${formattedUptime}\n`, inline: false },
+            { name: '🏠 Servers count', value: `${serverCount}\n`, inline: false },
+            { name: '💾 Memory use', value: `${formattedMemoryUsage}\n`, inline: false }
+        )
+        .setTimestamp();
 
         await channel.send({ embeds: [embed] });
-    } catch (error) {
-        console.error("Erro ao enviar atualização de status:", error);
-    }
-};
+        await channel.send({ embeds: [embed] });
+    await channel.send({ embeds: [embed] });
+}
 
 async function CheckChannelCriacoes() {
-    try {
-        const channel = await client.channels.fetch(chatID);
-        const messages = await channel.messages.fetch({ limit: 100 });
+    const channel = await client.channels.fetch(criacoesChatID);
+    const messages = await channel.messages.fetch({ limit: 100 });
 
-        for (const message of messages.values()) {
-            if (message.author.id === client.user.id || message.channelId !== chatID) continue;
-            await createThreadIfNeeded(message);
-        }
-
-        client.on("messageCreate", async (message) => {
-            if (message.author.id === client.user.id || message.channelId !== chatID) return;
-            await createThreadIfNeeded(message);
-        });
-    } catch (error) {
-        console.error("Erro ao verificar o canal de criações:", error);
+    for (const message of messages.values()) {
+        if (message.author.id === client.user.id || message.channelId !== criacoesChatID) continue;
+        await createThreadIfNeeded(message);
     }
+
+    client.on("messageCreate", async (message) => {
+        if (message.author.id === client.user.id || message.channelId !== criacoesChatID) return;
+        await createThreadIfNeeded(message);
+    });
 }
 
 async function startStatusUpdates() {
@@ -126,57 +125,49 @@ async function startStatusUpdates() {
 }
 
 async function countMentions(userId, channelId) {
-    try {
-        const channel = await client.channels.fetch(channelId);
-        const messages = await channel.messages.fetch({ limit: 100 });
-        let mentionCount = 0;
+    const channel = await client.channels.fetch(channelId);
+    const messages = await channel.messages.fetch({ limit: 100 });
+    let mentionCount = 0;
 
-        messages.forEach(message => {
-            if (message.mentions.has(userId)) {
-                mentionCount++;
-            }
-        });
-
+    messages.forEach(message => {
+        if (message.mentions.has(userId)) {
+            mentionCount++;
+        }
+    });
         return mentionCount;
-    } catch (error) {
-        console.error("Erro ao contar menções:", error);
-    }
 }
 
 async function handleReaction(reaction) {
+    const message = reaction.message;
+    const guild = message.guild
+    console.log(`Reação dada a mensagem de ${message.user}\nConteúdo da mensagem: ${message.content}`)
+
     try {
-        const message = reaction.message;
-        const guild = message.guild;
         const starEmoji = await guild.emojis.fetch(starEmojiID);
 
-        if (reaction.emoji.id === correctEmojiID && reaction.count >= 20 && !(message.reactions.cache.has(`<:yellowstar:${starEmojiID}>`) || message.reactions.cache.has('⭐️'))) {
-            
+        if (reaction.emoji.id === correctEmojiID && reaction.count >= 20 && !message.reactions.cache.has(starEmojiID)) {
+            console.log("Criando mensagem.")
             const destaquesChannel = await client.channels.fetch(destaquesChatID);
-
-            try {
-                await message.fetch();
-            } catch (error) {
-                console.error("A mensagem não foi encontrada ou foi deletada.", error);
-                return;
-            }
-
+    
             if (destaquesChannel) {
                 if (message.attachments.size > 0) {
-                    await destaquesChannel.send({
+                    console.log("Enviando mensagem")
+                    const messageSend = await destaquesChannel.send({
                         content: `# <@${message.author.id}>\n\n> - ${message.content || "*Sem descrição*"}`,
                         files: message.attachments.map(attachment => ({
                             attachment: attachment.url,
                             name: attachment.name
                         }))
                     });
-
-                    await message.react(`<:yellowstar:${starEmoji}>`);
-
+                    console.log("Adicionando reação a mensagem.")
+                    await message.react(starEmojiID);
+                    await messageSend.react(starEmojiID)
+    
                     const member = await message.guild.members.fetch(message.author.id);
-
+    
                     const mentionCount = await countMentions(message.author.id, destaquesChatID);
                     let roleId;
-
+    
                     switch (true) {
                         case (mentionCount >= 1 && mentionCount <= 4):
                             roleId = '1256153440325730324';
@@ -190,7 +181,7 @@ async function handleReaction(reaction) {
                         default:
                             roleId = null;
                     }
-
+    
                     if (roleId) {
                         try {
                             await member.roles.add(roleId);
@@ -202,7 +193,7 @@ async function handleReaction(reaction) {
             }
         }
     } catch (error) {
-        console.error("Erro ao tratar a reação:", error);
+        console.error('Erro ao buscar o emoji:', error);
     }
 }
 
@@ -218,10 +209,6 @@ client.on('messageReactionAdd', async (reaction, user) => {
     if (reaction.message.channelId === criacoesChatID) {
         await handleReaction(reaction);
     }
-});
-
-client.on('messageReactionRemove', async (reaction, user) => {
-    if (user.bot) return;
 });
 
 client.login(process.env.TOKEN);
