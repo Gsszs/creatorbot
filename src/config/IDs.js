99c7e7fd2cc692e9d77c3f6e278d4ad80c26e658
Gsszs <gabriel.selvenca@icloud.com>
2024-10-24 15:19:28 -0300

    Correção IDs

--- src/config/IDs.js
+++ src/config/IDs.js
@@ -1,9 +1,9 @@
-const correctEmojiID = '1121131644111';
-const incorrectEmojiID = '1121131644112';
-const starEmojiID = '1121131644113';
-const criacoesChatID = '1234567891011';
-const destaquesChatID = '1234567891012';
-const pingChat = '1234567891013';
+const correctEmojiID = '1298345056117198868';
+const incorrectEmojiID = '1298345092448260147';
+const starEmojiID = '1254476086532898958';
+const criacoesChatID = '1268306951696289802';
+const destaquesChatID = '1268306944657981443';
+const pingChat = '1295544141387927580';
 
 module.exports = {
     correctEmojiID,
